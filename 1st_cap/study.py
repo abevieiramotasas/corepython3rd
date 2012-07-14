f37b52c4ec8fddc9040e714bd72a56d3567b89ee
abelardo <abbbekgb@gmail.com>
2012-07-14 16:57:26 -0400

    finalizando capitulo 1

--- 1st_cap/study.py
+++ 1st_cap/study.py
@@ -53,6 +53,8 @@ patterns.append(Pattern(r'((?:ab)*)', 'abababab'))
 
 patterns.append(Pattern(r'.*?(abe*)c*\1', 'uahauhsabeeecccccabeee', 'uahsuhaabeecccccabe'))
 
+
+# obs: pode usar \g<name> tbm
 patterns.append(Pattern(r'(?P<grupo>abe*)c*(?P=grupo)', 'abeeecccabeee', 'abeeeccabe'))
 
 patterns.append(Pattern(r'ab*(?#mas que regex linda)ab*', 'abbbabb', 'ababbb'))
@@ -74,7 +76,8 @@ patterns.append(Pattern(r'(?<=abc)defg*', 'defggabcdefggg'))
 
 patterns.append(Pattern(r'(?<!abc)defg*', 'defggabcdefggg'))
 
-# nao sei usar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!! nao sei usar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# exemplo mt bom no core programming p.34
 patterns.append(Pattern(r'(<)?\w+(?(1)>)', '<abe>', 'abe', '<abe'))
 
 patterns.append(Pattern(r'\bab', 'abelardo', 'o abelardo eh legal', 'oab'))
@@ -82,6 +85,64 @@ patterns.append(Pattern(r'\bab', 'abelardo', 'o abelardo eh legal', 'oab'))
 patterns.append(Pattern(r'\Bab', 'abelardo', 'o abelardo eh legal', 'oab'))
 
 patterns.append(Pattern(r'(\w+)-(\d+)', 'ab-10', 'aaa-20'))
+
+# passando a flag re.I IGNORECASE
+patterns.append(Pattern(r'^([a-z]*)$', 'abe', 'Abe'))
+
+patterns.append(Pattern(r'^([a-z]*)(?i)$', 'abe', 'Abe'))
+
+# !!!!!!!!!!!!!!!!!!!!!!! nao entendo LOCALE !!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# re.M MULTILINE
+patterns.append(Pattern(r'^([a-z]*)$', 'abe\nabe'))
+
+patterns.append(Pattern(r'^([a-z]*)$(?m)', 'abe\nabe'))
+
+# re.S DOTALL obs: faz com que o . case com o \n tambem
+# obs: diferenca pro re.M
+#   re.M faz com que o inicio e o fim do match sejam a linha e nao todo o string
+#   re.S faz com que o . case com o \n tambem
+#   por isso no exemplo do re.S, o match foi em toda a string
+patterns.append(Pattern(r'^(.*)$', 'abe\nabe'))
+
+patterns.append(Pattern(r'^(.*)$(?s)', 'abe\nabe'))
+
+# re.X VERBOSE
+# obs: espaco eh representado por [ ]
+pattern_string_verbose = r"""# esta linha eh um comentario
+                            ^(.*)$
+                             # casa com tudo esse rapaz
+                             (?x)
+                             """
+patterns.append(Pattern(pattern_string_verbose, 'abeabebab'))
+
+############################ Named Group ###################################
+
+def named_group():
+    m = re.match(r'^(?P<fn>\w*) (?P<ln>\w*)$', 'Abelardo Vieira')
+    print("Primeiro nome :", m.group('fn'))
+    print("Segundo nome :", m.group('ln'))
+    
+########################## Substitution ##############################
+
+def subs():
+    # exemplo, trocando as bolas
+    #           pattern     substituto      string
+    original = 'bola1 bola2'
+    m = re.sub(r'(\w+) (\w+)',         r'\2 \1',             original)
+    print(original, m)
+    
+########################## Split ###########################
+# obs: tem um exemplo muito bom no core programming p.30
+def split_it_out():
+    data = '01-02-2012'
+    d_m_a = re.split('-', data)
+    # usando sub
+    print(d_m_a)
+    
+split_it_out()
+
+
 # exemplo com (?:...)
 
 # re.match(r'ab(?:ab)cc', 'ababcc').groups() 
