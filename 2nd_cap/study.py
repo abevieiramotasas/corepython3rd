c03d06e0594150dce649526fd65d55d619c0f6bf
abelardo <abbbekgb@gmail.com>
2012-07-15 21:12:21 -0400

    comecando cap 3

--- 2nd_cap/study.py
+++
@@ -1,4 +0,0 @@
-# sockets
-from socket import *
-
-tcp_socket = socket(AF_INET, SOCK_STREAM)
--- 2nd_cap/tcp_server.py
+++ 2nd_cap/tcp_server.py
@@ -15,6 +15,7 @@ tcp_ser_s = socket(AF_INET, SOCK_STREAM)
 tcp_ser_s.bind(ADDR)
 # maximo numero de conexoes aceitas(fila de espera)
 tcp_ser_s.listen(5)
+setdefaulttimeout(2)
 
 while True:
     print 'waiting for connection ...'
@@ -23,10 +24,14 @@ while True:
     
     # tcp_cli_s.send("Testando")
     while True:
-        data = tcp_cli_s.recv(BUFSIZ)
-        if not data:
+        try:
+            data = tcp_cli_s.recv(BUFSIZ)
+            if not data:
+                break
+            tcp_cli_s.send('[%s] %s' % (ctime(), data))
+            print ">>>>", data
+        except timeout:
+            # deu timeout, morreu conexao
             break
-        tcp_cli_s.send('[%s] %s' % (ctime(), data))
-        print ">>>>", data
     tcp_cli_s.close()
 tcp_ser_s.close()
